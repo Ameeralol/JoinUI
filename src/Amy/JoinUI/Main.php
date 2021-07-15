6620ad6ce18314f29d5277384b2dd419f75e2b80
Ameeralol <87492744+Ameeralol@users.noreply.github.com>
2021-07-15 20:12:02 +0300

    Initial commit

---
+++ src/Amy/JoinUI/Main.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Amy\JoinUI;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+
+class Main extends PluginBase implements Listener{
+
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+	public function onJoin(PlayerJoinEvent $event){
+
+		$api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+        $form = $api->createSimpleForm(function (Player $player, int $data = null) {
+            $result = $data;
+            if ($result === null) {
+                return true;
+            }
+            switch($result){
+                case 0:
+                break;
+            }
+
+        });
+        $form->setTitle("JoinUI");
+		$form->setContent("JoinUI");
+        $form->addButton("Close");
+        $form->sendToPlayer($player);
+        return true;
+
+	}
+}
